ec7e85ef73b843fa405579b3f0920302f9fddbad
phillip.lawniczak <phillip.lawniczak@olx.pl>
2024-03-11 16:44:25 +0100

    add config for project plugin

--- init.lua
+++ init.lua
@@ -272,7 +272,10 @@ require('lazy').setup {
       {
         'ahmedkhalf/project.nvim',
         config = function()
-          require('project_nvim').setup {}
+          require('project_nvim').setup {
+            detection_methods = { 'pattern' },
+            patterns = { '.git' },
+          }
         end,
       },
     },
