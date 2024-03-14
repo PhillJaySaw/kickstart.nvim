056c06a2eb359f29a0d8da71a79f3ed42e6b4cae
phillip.lawniczak <phillip.lawniczak@olx.pl>
2024-03-14 17:13:49 +0100

    Add conditio to autosave config

--- lua/custom/plugins/autosave.lua
+++ lua/custom/plugins/autosave.lua
@@ -1,6 +1,17 @@
 return {
   {
     'okuuva/auto-save.nvim',
-    opts = {},
+    opts = {
+      condition = function(buf)
+        local buf = vim.api.nvim_get_current_buf()
+        local buf_modified = vim.api.nvim_buf_get_option(buf, 'modified')
+
+        if buf_modified then
+          return true
+        end
+
+        return false
+      end,
+    },
   },
 }
