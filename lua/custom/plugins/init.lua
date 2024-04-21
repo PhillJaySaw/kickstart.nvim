208e9dfce3ce4b4b7295bffbcfc011beae961fb9
phillip.lawniczak <phillip.lawniczak@olx.pl>
2024-04-21 22:55:19 +0200

    add tmux config to nvim, and add some plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -55,4 +55,22 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {},
   },
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
+  { 'dmmulroy/tsc.nvim', config = true },
 }
--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -6,11 +6,11 @@ return {
       require('lualine').setup {
         sections = {
           lualine_a = { 'mode' },
-          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_b = { 'diagnostics' },
           lualine_c = { { 'filename', path = 1 } },
           lualine_x = { 'encoding', 'filetype' },
           lualine_y = { 'progress', 'location' },
-          lualine_z = { 'ctime' },
+          lualine_z = {},
         },
         inactive_sections = {
           lualine_a = {},
