14a60c4ad5b1f9f47751e165e547b56cb0e7744c
phillip.lawniczak <phillip.lawniczak@olx.pl>
2024-07-15 00:13:30 +0200

    add harpoon

--- lua/custom/plugins/autosave.lua
+++ lua/custom/plugins/autosave.lua
@@ -1,6 +1,13 @@
 return {
   {
     'okuuva/auto-save.nvim',
-    opts = {},
+    opts = {
+      condition = function(buf)
+        if vim.bo[buf].filetype == 'harpoon' then
+          return false
+        end
+        return true
+      end,
+    },
   },
 }
