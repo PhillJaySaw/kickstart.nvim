5f1a2899837c3f95aba34e98093ea89d7c8bc4d3
philliplawniczak <philljaysaw@gmail.com>
2025-11-05 22:23:23 +0100

    update treesitter ensure installed

--- init.lua
+++ init.lua
@@ -1025,7 +1025,23 @@ require('lazy').setup {
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'css', 'styled', 'graphql' },
+        ensure_installed = {
+          'bash',
+          'c',
+          'diff',
+          'html',
+          'lua',
+          'luadoc',
+          'markdown',
+          'markdown_inline',
+          'query',
+          'vim',
+          'vimdoc',
+          'css',
+          'styled',
+          'graphql',
+          'python',
+        },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
